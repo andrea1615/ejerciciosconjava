47fb40f80df73cae8bfcc2cacbbcdba2d30afca2
andrea1615 <andreagracia1615@gmail.com>
2024-08-13 17:59:13 -0600

    ejercicios del 13 de Agosto

---
+++ src/reto03/reto03.java
@@ -0,0 +1,11 @@
+package reto03;
+
+public class reto03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Paula Andrea";
+		 System.out.println("Nombre: " + nombre);
+	}
+
+}
---
+++ src/reto04/reto04.java
@@ -0,0 +1,21 @@
+package reto04;
+
+public class reto04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declaración de variables
+        String nombre = "Paula Andrea"; 
+        String direccion = "Pelicanos 3"; 
+        String telefono = "3154774212"; 
+
+        // Mostrar valores de las variables por pantalla
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Mi dirección es: " + direccion);
+        System.out.println("Mi teléfono es: " + telefono);
+
+
+	}
+
+}
